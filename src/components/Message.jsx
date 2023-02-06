2295df94974826357ad2a765668b67af426dd5bc
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2023-02-06 15:06:13 +0200

    cmt5

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import Pers from '../img/olgarepina.jpg'
+
 const Message = () => {
   return (
-    <div>
-      Message
+    <div className='message'>
+      <div className="messageInfo">
+        <img src={Pers} alt="" />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src={Pers} alt="" />
+      </div>
     </div>
   )
 }
